a7a285f659b21739f450a5abe0b1b21436fb0fdc
M3ttle <nonni@nocando.is>
2017-10-15 03:48:06 +0000

    Add function: Unit tests fixed so they should expect the correct values

--- src/test/java/is/ru/stringcalculator/StringCalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/StringCalculatorTest.java
@@ -52,12 +52,12 @@ public class StringCalculatorTest{
 	@Test
 	public void addThreeNumbersNewLine1(){
 		StringCalculator calc = new StringCalculator();
-		assertEquals(7, calc.Add("1\n2,3"));
+		assertEquals(6, calc.Add("1\n2,3"));
 	}
 	@Test
 	public void addThreeNumbersNewLine2(){
 		StringCalculator calc = new StringCalculator();
-		assertEquals(9, calc.Add("5\n1\n4"));
+		assertEquals(10, calc.Add("5\n1\n4"));
 	}
 
 }
