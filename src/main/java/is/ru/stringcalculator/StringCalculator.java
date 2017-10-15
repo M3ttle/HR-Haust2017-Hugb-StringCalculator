faf8aab669703c72db4ba37476455fa93a37bf77
M3ttle <nonni@nocando.is>
2017-10-15 03:32:59 +0000

    Add function: Refactoring the function since getting the values are correct

--- src/main/java/is/ru/stringcalculator/StringCalculator.java
+++ src/main/java/is/ru/stringcalculator/StringCalculator.java
@@ -10,12 +10,15 @@ public class StringCalculator{
 			return 0;
 		}
 		// If numbers is not empty we continue.
-		String[] splittedNumbers = numbers.split(",");
+		return getSum(numbers);
+	}
+
+	private int getSum(String number){
+		String[] splittedNumbers = number.split(",");
 		int sum = 0;
 		for(String numb : splittedNumbers){
 			sum += Integer.parseInt(numb);
 		}
-
 		return sum;
 	}
 }
